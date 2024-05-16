57c179e3c2ec25e2fce309b68ba48b57aca2e00c
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2024-05-17 04:31:41 +0700

    fix

--- commands/functions/reg_apple_id.py
+++ commands/functions/reg_apple_id.py
@@ -250,7 +250,7 @@ def add_payment(browser, data, apple):
         
         data_card = db_instance.fetch_data(table_name="pay", columns=["*"], condition="status = 1 and on_use = 0 limit 1")
         print(data_card[0])
-        db_instance.update_data(table_name="pay", data={"on_use": 1}, condition="id = %s", values=[data_card[0][0]])
+        db_instance.update_data(table_name="pay", condition=f'id = {data_card[0][0]}', data={"on_use": 1})
         print(data_card[0])
         try:
             if data_card[0] is None:
