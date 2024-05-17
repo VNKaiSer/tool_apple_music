ee1f6f07be4f22940588744016afc4275dca97a9
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2024-05-17 22:13:07 +0700

    fix

--- commands/functions/reg_apple_id.py
+++ commands/functions/reg_apple_id.py
@@ -157,7 +157,7 @@ def apple_id_done(browser, data):
 
 def check_account_is_block(browser):
     try:
-        browser.switch_to.default_content()
+        # browser.switch_to.default_content()
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'aid-auth-widget-iFrame')))
         iframe = browser.find_element(By.ID, 'aid-auth-widget-iFrame')
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'h2')))
