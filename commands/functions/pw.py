f53032be0c607466fabb5224896fab5201960939
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2024-05-21 21:52:12 +0700

    feat: add check ip

--- commands/functions/pw.py
+++ commands/functions/pw.py
@@ -1,27 +1,29 @@
+from seleniumwire import webdriver
 import json
-import random
+def check_region(browser):
+    try: 
+        browser.get('http://ip-api.com/json/')
+        for request in browser.requests:
+            if request.response and 'ip-api.com/json' in request.url:
+                response_body = request.response.body.decode('utf-8')
+                data = json.loads(response_body)
+                if data['countryCode'] != 'US':
+                    browser.quit()
+                    return False
+        browser.quit()
+        return True
+    except Exception as e:
+        browser.quit()
+        return False
+# Cấu hình Selenium Wire
+options = {
+    'proxy':  
+            {
+                'https': 'https://brd-customer-hl_d346dd25-zone-static-country-us:jmkokxul20oa@brd.superproxy.io:22225',
+                'http': 'http://brd-customer-hl_d346dd25-zone-static-country-us:jmkokxul20oa@brd.superproxy.io:22225',
+                'no_proxy': 'localhost,127.0.0.1'
+            }
+}
 
-def random_address():
-    json_file = './assets/data/addresses.json'  
-    with open(json_file, 'r') as f:
-        data = json.load(f)
+print(check_region(webdriver.Firefox(seleniumwire_options=options)))
 
-    # Lấy danh sách các địa chỉ từ khóa 'addresses'
-    addresses = data.get('addresses', [])
-
-    # Lựa chọn ngẫu nhiên một địa chỉ từ danh sách
-    while True:
-        random_address = random.choice(addresses)
-        # Kiểm tra xem có bất kỳ trường nào bị thiếu không
-        if all(key in random_address for key in ['address1', 'address2', 'city', 'state', 'postalCode']):
-            break  # Nếu không thiếu trường nào, thoát khỏi vòng lặp
-
-    return random_address['address1'], random_address['address2'], random_address['city'], random_address['state'], random_address['postalCode']
-
-# Sử dụng hàm để lấy địa chỉ ngẫu nhiên
-address1, address2, city, state, postal_code = random_address()
-print("Address 1:", address1)
-print("Address 2:", address2)
-print("City:", city)
-print("State:", state)
-print("Postal Code:", postal_code)
