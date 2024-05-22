be86409e57b12ed45ea3f233068cb0b47d913209
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2024-05-22 22:17:50 +0700

    fixx

--- commands/reg_apple_tv_add.py
+++ commands/reg_apple_tv_add.py
@@ -1,5 +1,15 @@
 print("Đang REG tài khoản Apple TV và thêm thẻ")
+import json
+import time
 from functions import reg_apple_tv as reg
 
 
-# reg
+def check_run_app():
+    f = open ('./config/tool-config.json', "r")
+    data = json.loads(f.read())
+    f.close()
+    return data['RUN']
+
+while check_run_app():    
+    reg.reg_apple_tv()
+    time.sleep(3)
