8037ebd1dc43790f344a5de859733196d254c494
VNKaiser <tandatvok16@gmail.com>
2024-09-30 16:11:46 +0700

    feat: sửa lỗi side

--- commands/functions/sideline.py
+++ commands/functions/sideline.py
@@ -387,6 +387,7 @@ def login(change_password = False, send_message = False, delete_message = False,
                 WebDriverWait(app_root, 15).until(EC.visibility_of_element_located((By.CLASS_NAME, 'error-message')))
                 wrong_password = driver.find_element(By.CLASS_NAME, 'error-message').text
                 if wrong_password != "":
+                    err = ""
                     if check_live == True:
                         err = "invalid phone" if wrong_password == "Please enter a valid phone number." else "sai pass"
                         if err == "sai pass":
@@ -400,6 +401,7 @@ def login(change_password = False, send_message = False, delete_message = False,
                                 err = "invalid phone"
                         else:
                             err = "sai pass"
+                    print(err)
                     if not change_password:
                         db_instance.result_acc_sideline(username, err)
                     else:
